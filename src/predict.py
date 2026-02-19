bd10c085893e73f6fbb07ffef5e1ac8f6e653a16
Tomas Vincent <91481444+UP933272@users.noreply.github.com>
2026-02-19 18:24:03 +0000

    all updated

--- src/predict.py
+++ src/predict.py
@@ -8,8 +8,12 @@ vectorizer = pickle.load(open("../models/vectorizer.pkl", "rb"))
 print("----------------------------------")
 print(" Fake News Detection Tool")
 print("----------------------------------")
+print("\n")
+print("Welcome to the fake news detection tool")
 
-text = input("\nEnter text to analyse:\n> ")
+text = input("Please enter text from a social media post to scan it for fake news:\n")
+
+
 
 print("\nScanning...")
 time.sleep(2)
@@ -21,6 +25,6 @@ text_vectorized = vectorizer.transform([text])
 prediction = model.predict(text_vectorized)[0]
 
 if prediction == 0:
-    print("\nResult: FAKE NEWS ❌")
+    print("\nResult: This is fake news ❌")
 else:
-    print("\nResult: REAL NEWS ✅")
+    print("\nResult: This is not fake news ✅")
